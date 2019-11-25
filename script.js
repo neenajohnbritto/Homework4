43ddeed4d6907df119188864438c96f05d3f9a58
njohnbritto <nirmaljulietneena.johnbritto@charter.com>
2019-11-24 17:21:32 -0700

    Finished Homework4

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ var questions = [
 
 ];
 
-var isAnswerValidated = false;
+var score = 0;
 
 
 var mainContainer = document.querySelector(".mainContainer");
@@ -39,87 +39,41 @@ var startQuiz = document.querySelector("#startQuiz");
 var timer = document.querySelector("#timer");
 var quizHeader = document.querySelector(".quizHeader");
 var quizDescription = document.querySelector("#quizDescription");
+var viewHighScores = document.querySelector("#viewHighScores");
 
 var secondsLeft = questions.length * 15;
+var index = 0;
+
+viewHighScores.addEventListener("click", function() {
+  console.log("Am viewHighScores!!!")
+  quizHeader.remove();
+  quizDescription.remove();
+  startQuiz.remove();
+  displayHighScores();
+});
 
 startQuiz.addEventListener("click", function() {
   console.log("Am here!!!")
   quizHeader.remove();
   quizDescription.remove();
   startQuiz.remove();
-  
-  for(var i=0; i< 1; i++){
-    console.log("Now here");
-    var h5 = document.createElement("h5");
-    h5.textContent = questions[i].title;
-    mainContainer.appendChild(h5);
-    h5.setAttribute("id", "questionTag");
-  
-    var liOne = document.createElement("ol");
-    mainContainer.appendChild(liOne);
-    var buttonOne = document.createElement("button");
-    buttonOne.textContent = questions[i].choices[0];
-    liOne.appendChild(buttonOne);
-  
-    var liTwo = document.createElement("ol");
-    mainContainer.appendChild(liTwo);
-    var buttonTwo = document.createElement("button");
-    buttonTwo.textContent = questions[i].choices[1];
-    liTwo.appendChild(buttonTwo);
-  
-    var liThree = document.createElement("ol");
-    mainContainer.appendChild(liThree);
-    var buttonThree = document.createElement("button");
-    buttonThree.textContent = questions[i].choices[2];
-    liThree.appendChild(buttonThree);
-  
-    var liFour = document.createElement("ol");
-    mainContainer.appendChild(liFour);
-    var buttonFour = document.createElement("button");
-    buttonFour.textContent = questions[i].choices[3];
-    liFour.appendChild(buttonFour);
 
+  nextQuestion();
    
-
-    var answerButtons = document.getElementsByTagName("button");
-    for(var j=0; j<answerButtons.length;j++){
-      var selectedAnswer = questions[i].choices[j];
-      var correctAnswer = questions[i].answer;
-      answerButtons[j].onclick = function() { 
-        verifyAnswer(selectedAnswer, correctAnswer);
-      };
-      
-      
-    }
-    if(isAnswerValidated){
-      clearQuestion();
-    }
-    
-    
-  
-    
-  }
- //Start Timer and run up to 75 seconds 
- 
-    var interval = setInterval(function() {
-      
-
-      
-      
-
-      
+  var interval = setInterval(function() {
       if (secondsLeft === 0) {
-        clearInterval(interval)
-        console.log('BOOM')
+        clearInterval(interval);
+        console.log('BOOM');
+        clearQuestion();
+        displayResult();
       }
       timer.textContent = "Time : " + secondsLeft;
       secondsLeft--;
-    }, 1000)
+  }, 1000)
  //Loop through questions
  //Calculate Score
-
-
 });
+
 
 function verifyAnswer(selectedAns, correctAns){
   console.log("Inside Verify Answer. selectedAns: " + selectedAns + " correctAns: " + correctAns);
@@ -137,57 +91,147 @@ function verifyAnswer(selectedAns, correctAns){
     var pOne = document.createElement("p");
     pOne.textContent = result;
     liFive.appendChild(pOne);
-    isAnswerValidated = true;
-
     
+
+    if(result === "Correct"){
+      score = score + (100 / questions.length);
+    }
+    else{
+      score = score - 5;
+      secondsLeft = secondsLeft - 5;
+    }
+    //clearQuestion();    
 }
+
 
 function clearQuestion(){
-  var allOls = document.getElementsByTagName("ol");
-  var allH5s = document.getElementsByTagName("h5");
-  for(var i=0; i < allOls.length; i++){
-    allOls[i].remove();
-  }
-  allH5s[0].remove();
-  isAnswerValidated = false;
+  $("h5").remove();
+  $("ol").remove();
 }
 
 
-// var i = 0;
+function nextQuestion(){
+  var h5 = document.createElement("h5");
+  mainContainer.appendChild(h5);
+  h5.setAttribute("id", "questionTag");
+  
+  var liOne = document.createElement("ol");
+  mainContainer.appendChild(liOne);
+  
+  h5.textContent = questions[index].title;
+  var correctAnswer = questions[index].answer;
+    
+for (let i = 0; i < questions[index].choices.length; i++) {
+  console.log(questions[index].choices[i]);
+  var liOne = document.createElement("ol");
+  mainContainer.appendChild(liOne);
+  var buttonC = document.createElement("button");
+  buttonC.setAttribute("id", "button" + i);
+  liOne.appendChild(buttonC);
+  buttonC.textContent = questions[index].choices[i];
+}
 
-// var wordsPerMillisecond = prompt("How many words per millisecond would you like to read?");
+$("button").on("click", function(){
+  var selectedAnswer = this.textContent;
+  console.log(selectedAnswer);
+  verifyAnswer(selectedAnswer, correctAnswer);
+  setTimeout(function(){ 
+    console.log("index: " , index);
+    index ++;
+    if(index < questions.length){
+      clearQuestion();
+      nextQuestion();
+      console.log("index: " , index);
+    }
+    else{
+      clearQuestion();
+      displayResult();
+      
+    }
+  }, 1000);
+  });
+}
 
-// function prepareRead() {
-//   var timeLeft = 2;
+function displayResult(){
 
-//   var timeInterval = setInterval(function() {
-//     timerEl.textContent = timeLeft + " seconds remaining";
-//     timeLeft--;
+  var h4 = document.createElement("h4");
+  mainContainer.appendChild(h4);
+  h4.textContent = "All Done!";
+  
+  var h5 = document.createElement("h5");
+  mainContainer.appendChild(h5);
+  h5.textContent = "Your Score is: " + score;
 
-//     if (timeLeft === 0) {
-//       timerEl.textContent = "";
-//       speedRead();
-//       clearInterval(timeInterval);
-//     }
+  var form = document.createElement("form");
+  mainContainer.appendChild(form);
+  form.textContent = "Enter initial to save your score: ";
+  form.setAttribute("id", "form1");
+  document.getElementById("form1").innerHTML="<input type='text' value='' id='initials'>";
+  
+  var button = document.createElement("button");
+  mainContainer.appendChild(button);
+  button.setAttribute("id", "buttonSave");
+  button.textContent = "Save Scores";
+  
+  //var addScores = document.getElementById("addScores");
+  $("#buttonSave").on("click", function() {
+    var initials = document.getElementById("initials").value;
+    
+    console.log(initials);
+    localStorage.setItem(initials, score);
 
-//   }, 1000);
-// }
+    h4.remove();
+    h5.remove();
+    form.remove();
+    button.remove();
+    displayHighScores();
+    });
+
+}
+
+function displayHighScores(){
+  
+
+  var h4 = document.createElement("h4");
+  mainContainer.appendChild(h4);
+  h4.textContent = "High Scores";
+
+  var keys = Object.keys(localStorage);
+  
+
+  for(var i=0; i<keys.length; i++){
+    var p = document.createElement("p");
+    mainContainer.appendChild(p);
+    p.textContent = keys[i] + "   " + window.localStorage.getItem(keys[i]);
+  }
+
+  var button = document.createElement("button");
+  mainContainer.appendChild(button);
+  button.setAttribute("id", "buttonGoBack");
+  button.textContent = "Go Back";
+
+  var button = document.createElement("button");
+  mainContainer.appendChild(button);
+  button.setAttribute("id", "buttonClearScores");
+  button.textContent = "Clear Scores";
+
+  $("#buttonGoBack").on("click", function() {
+    location.reload();
+  });
+
+  $("#buttonClearScores").on("click", function() {
+    window.localStorage.clear();
+    $("h4").remove();
+    $("p").remove();
+    $("button").remove();
+    displayHighScores();
+  });
 
 
-// function speedRead() {
-//   mainEl.append(bodyEl);
+}
 
-//   var poemInterval = setInterval(function() {
-//     if (words[i] === undefined) {
-//       clearInterval(poemInterval);
-//     } else {
-//       mainEl.textContent = words[i];
-//       i++;
-//     }
 
-//   }, wordsPerMillisecond);
-// }
 
-// prepareRead();
+
 
 
